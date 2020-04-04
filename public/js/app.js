37775be9901041997e12046a00c3dc64258f8789
Nitin2 Dixit <Nitin2.Dixit@ril.com>
2020-04-04 18:42:15 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("JS File");
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const message1 = document.querySelector('#message-1');
